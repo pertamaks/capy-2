5aae77383dec17efef96aa53126ca81794837d9e
pertamaks <andreasady.nugroho@gmail.com>
2019-05-02 16:11:51 +0700

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,11 +4,13 @@ require 'capybara/session'
 require 'capybara/dsl'
 require 'capybara-screenshot/cucumber'
 
+#handler
 Capybara.default_driver = :chrome
 Capybara.javascript_driver = :chrome
 Capybara.default_max_wait_time = 20
 Capybara.save_path = "E:\\GIT AUTO\\capy 2\\screenshot"
 
+#handler set-up
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_preference(:download, prompt_for_download: false,
                                   default_directory: '/tmp/downloads')
